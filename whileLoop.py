3d59e89b78ed6ccbb70a6812b8fb4d6bb6e4c29a
UltraxBlade <mjrabinowitz01@gmail.com>
2018-05-05 11:57:00 -0400

    Add files via upload

---
+++ whileLoop.py
@@ -0,0 +1,55 @@
+input("pause")
+
+i=0
+while i<10:
+    print(i)
+    i+=1
+
+input("pause")
+
+i=0
+while i<10:
+    print(i)
+    i+=2
+
+input("pause")
+
+i=1
+while i<10:
+    print(i)
+    i+=2
+
+input("pause")
+
+i=1
+while i<3000:
+    print(i)
+    i=i*2
+
+input("pause")
+
+m=0
+while m<5:
+    print("hello")
+    m+=1
+
+input("pause")
+
+go=True
+while go:
+    if input("stop?")=="yes":
+        go=False
+    else:
+        print("go")
+
+input("pause")
+
+while input("stop?")!="yes":
+    print("go")
+
+input("pause")
+
+n=0
+while n<2:
+    print ("kaboom!")
+
